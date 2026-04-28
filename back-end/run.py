574414adb6effe3e593f149a9f3c744fcf567cb4
joaovi3137_00 <joao.gomes@sicoobcredicaf.com.br>
2026-04-28 09:07:29 -0300

    Adição de docker e criação do banco de dados

--- back-end/run.py
+++ back-end/run.py
@@ -1,9 +1,11 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
+    reload = os.getenv("ENV", "production") == "development"
     uvicorn.run(
-        "main.server.server:app", 
-        host="0.0.0.0", 
+        "main.server.server:app",
+        host="0.0.0.0",
         port=8000,
-        reload=True
+        reload=reload,
     )
